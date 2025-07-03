915f1f9779014a081808f13197b8eff4bbca0cbb
“Haruka” <harusaki812@icloud.com>
2025-07-03 17:21:36 +0900

    Add test branch file

--- HelloSwiftU/ContentView.swift
+++ HelloSwiftU/ContentView.swift
@@ -37,7 +37,7 @@ struct ContentView: View {
         NavigationView {
             VStack(spacing: 10) {
                 // アプリのタイトル
-                Text("To Do List 🛒")
+                Text("To Do 🛒")
                     .font(.title2)
                     .fontWeight(.bold)
                     .padding(.top, 2)
